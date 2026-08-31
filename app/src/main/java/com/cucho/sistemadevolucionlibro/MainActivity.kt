28071712132aa63d0626965496d2f64e3a0acc6c
luis <luis.cucho@tecsup.edu.pe>
2026-08-31 00:19:08 -0500

    Crea proyecto inicial del sistema de biblioteca

---
+++ app/src/main/java/com/cucho/sistemadevolucionlibro/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cucho.sistemadevolucionlibro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cucho.sistemadevolucionlibro.ui.theme.SistemaDevolucionLibroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SistemaDevolucionLibroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SistemaDevolucionLibroTheme {
+        Greeting("Android")
+    }
+}
